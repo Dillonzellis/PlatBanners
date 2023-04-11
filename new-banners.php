38cd4f428ab59b875bd9ca8aede7ee83e458544b
Dillon Ellis <dillonzellis@gmail.com>
2023-04-11 11:59:58 -0400

    add button hover text color

--- new-banners.php
+++ new-banners.php
@@ -112,36 +112,112 @@
         <!-- Additional required wrapper -->
         <div class="swiper-wrapper">
           <!-- Slides -->
+          <!-- Annual Meeting -->
           <div
             class="swiper-slide min-h-350 text-shadow tw-bg-brandingGreen-400"
           >
             <div class="tw-mx-auto tw-flex tw-max-w-[1500px]">
               <div
-                class="content tw-absolute tw-top-1/2 tw-z-50 tw-flex tw-w-full tw-max-w-[50rem] tw--translate-y-1/2 tw-flex-col tw-gap-2 tw-px-2 tw-text-center tw-text-white md:tw-top-1/2 md:tw--translate-y-1/2 md:tw-gap-y-4 md:tw-px-8 md:tw-text-left"
+                class="content tw-absolute tw-top-1/2 tw-z-50 tw-flex tw-w-full tw-max-w-[50rem] tw--translate-y-1/2 tw-flex-col tw-gap-2 tw-px-2 tw-text-center tw-font-serif tw-text-white md:tw-top-1/2 md:tw--translate-y-1/2 md:tw-gap-y-2 md:tw-px-8 md:tw-text-left xl:tw-gap-y-4"
+              >
+                <div class="md:tw-flex md:tw-flex-col md:tw-gap-y-2">
+                  <div
+                    class="tw-text-xl tw-font-semibold md:tw-text-lg md:tw-leading-4 lg:tw-text-3xl 2xl:tw-text-5xl"
+                  >
+                    Sunday April 23rd
+                  </div>
+                  <div
+                    class="tw-pb-4 tw-text-xl tw-font-semibold md:tw-text-xl md:tw-leading-4 lg:tw-text-3xl 2xl:tw-text-5xl"
+                  >
+                    Ashiana Banquet Hall
+                  </div>
+                  <div
+                    class="tw-text-base tw-font-semibold md:tw-text-lg md:tw-leading-4 lg:tw-text-2xl 2xl:tw-text-4xl"
+                  >
+                    5675 Jimmy Carter Blvd, Norcross, GA 30071
+                  </div>
+                  <div
+                    class="tw-text-base tw-font-semibold md:tw-text-lg md:tw-leading-4 lg:tw-text-2xl 2xl:tw-text-4xl"
+                  >
+                    12:00 PM
+                  </div>
+                </div>
+                <div class="tw-text-base xl:tw-text-2xl">
+                  Members who RSVP and arrive on time
+                  <br class="tw-hidden md:tw-block xl:tw-hidden" />
+                  will be entered into a drawing for a prize.
+                </div>
+                <div class="xl:tw-mt-4">
+                  <a
+                    class="text-shadow-0 tw-inline-flex tw-items-center tw-rounded-md tw-bg-brandingRed-400 tw-px-2 tw-py-2 tw-font-bold tw-text-white tw-shadow-md tw-transition-all tw-ease-in-out hover:tw-scale-110 hover:tw-text-white md:tw-text-lg"
+                    href="mailto:agm@platinumfcu.org"
+                    >RSVP agm@platinumfcu.org</a
+                  >
+                </div>
+              </div>
+            </div>
+            <div
+              class="overlay tw-absolute tw-z-10 tw-h-full tw-w-full tw-bg-black/75 md:tw-hidden"
+            ></div>
+            <img
+              src="./images/swiper-banner/annual-meeting-dt.jpg"
+              class="tw-hidden md:tw-block"
+              alt=""
+              srcset=""
+            />
+            <div
+              class="[mobile] tw-absolute tw-flex tw-h-full tw-w-full tw-items-center"
+            >
+              <img
+                src="./images/swiper-banner/annual-meeting-mobile.jpg"
+                class="tw-h-full tw-w-full tw-object-cover md:tw-hidden"
+                alt=""
+                srcset=""
+              />
+            </div>
+          </div>
+          <!-- MWM Loan Banner -->
+          <div
+            class="swiper-slide min-h-350 text-shadow tw-bg-brandingGreen-400"
+          >
+            <div class="tw-mx-auto tw-flex tw-max-w-[1500px]">
+              <div
+                class="content tw-absolute tw-top-1/2 tw-z-50 tw-flex tw-w-full tw-max-w-[50rem] tw--translate-y-1/2 tw-flex-col tw-gap-2 tw-px-2 tw-text-center tw-text-white md:tw-top-1/2 md:tw--translate-y-1/2 md:tw-gap-y-2 md:tw-px-8 md:tw-text-left lg:tw-gap-y-4"
               >
                 <div
-                  class="tw-font-serif tw-text-2xl tw-font-semibold md:tw-text-3xl lg:tw-text-5xl"
+                  class="tw-pb-2 tw-font-serif tw-text-2xl tw-font-semibold tw-capitalize md:tw-text-2xl lg:tw-text-4xl xl:tw-pb-4 xl:tw-text-5xl"
                 >
-                  Navros Ceritificate of Deposits Special
-                </div>
-                <div class="tw-font-serif tw-text-lg lg:tw-text-2xl">
-                  Welcome the New Year with a Limited Time CD Special
+                  Make your Money go the distance
                 </div>
                 <div
-                  class="tw-font-serif tw-text-2xl tw-font-black md:tw-text-3xl lg:tw-text-5xl"
+                  class="tw-flex tw-flex-col tw-pb-2 lg:tw-gap-2 xl:tw-pb-4"
                 >
-                  4.06% <span class="lg:tw-text-2xl">APY*</span>
+                  <div class="tw-font-serif tw-text-lg lg:tw-text-xl">
+                    Retirement & Income Strategies
+                  </div>
+                  <div class="tw-font-serif tw-text-lg lg:tw-text-xl">
+                    Investment Analysis & Portfolio Allocation
+                  </div>
+                  <div class="tw-font-serif tw-text-lg lg:tw-text-xl">
+                    Wealth Transfer& Estate Planning Strategies
+                  </div>
                 </div>
-                <div class="tw-hidden tw-font-serif md:tw-text-2xl xl:tw-block">
-                  Choose your term through March 31, 2023!
-                </div>
-                <div class="tw-hidden tw-font-serif md:tw-text-xl">
-                  9 months or 18 months
-                </div>
-                <div class="">
+
+                <div
+                  class="tw-font-serif tw-capitalize lg:tw-text-2xl"
+                >
                   <a
-                    class="text-shadow-0 tw-inline-flex tw-items-center tw-rounded-md tw-bg-brandingRed-400 tw-px-2 tw-py-2 tw-font-bold tw-capitalize tw-text-white tw-shadow-md tw-transition-all tw-ease-in-out hover:tw-scale-110 md:tw-text-lg"
-                    href="http://"
+                    href="mailto:mwm@platinumfcu.org"
+                    class="tw-font-bold tw-underline"
+                    >Email Us</a
+                  >
+                  to schedule your appointment today!
+                </div>
+
+                <div class="lg:tw-mt-4">
+                  <a
+                    class="text-shadow-0 tw-inline-flex tw-items-center tw-rounded-md tw-bg-brandingRed-400 tw-px-2 tw-py-2 tw-font-bold tw-capitalize tw-text-white tw-shadow-md tw-transition-all tw-ease-in-out hover:tw-scale-110 hover:tw-text-white md:tw-text-lg"
+                    href="https://www.platinumfcu.org/pages/services-general.php"
                     target="_blank"
                     rel="noopener noreferrer"
                     >learn more</a
@@ -153,7 +229,7 @@
               class="overlay tw-absolute tw-z-10 tw-h-full tw-w-full tw-bg-black/75 md:tw-hidden"
             ></div>
             <img
-              src="./images/swiper-banner/feb-banner.jpg"
+              src="./images/swiper-banner/MWM-loan-banner.jpg"
               class="tw-hidden tw-w-full md:tw-block"
               alt=""
               srcset=""
@@ -162,36 +238,83 @@
               class="[mobile] tw-absolute tw-flex tw-h-full tw-w-full tw-items-center"
             >
               <img
-                src="./images/swiper-banner/plat feb img isolate 1.png"
+                src="./images/swiper-banner/MWM-loan-mobile.jpg"
                 class="tw-h-full tw-w-full tw-object-cover md:tw-hidden"
                 alt=""
                 srcset=""
               />
             </div>
           </div>
+          <!-- Student Loans Banner -->
           <div
             class="swiper-slide min-h-350 text-shadow tw-bg-brandingGreen-400"
           >
             <div class="tw-mx-auto tw-flex tw-max-w-[1500px]">
               <div
-                class="content tw-absolute tw-top-1/2 tw-z-50 tw-flex tw-w-full tw-max-w-[50rem] tw--translate-y-1/2 tw-flex-col tw-gap-2 tw-px-2 tw-text-center tw-text-white md:tw-top-1/2 md:tw--translate-y-1/2 md:tw-gap-y-4 md:tw-px-8 md:tw-text-left"
+                class="content tw-absolute tw-top-1/2 tw-z-50 tw-flex tw-w-full tw-max-w-[50rem] tw--translate-y-1/2 tw-flex-col tw-gap-2 tw-px-2 tw-text-center tw-font-serif tw-text-white md:tw-top-1/2 md:tw--translate-y-1/2 md:tw-gap-y-4 md:tw-px-8 md:tw-text-left"
               >
                 <div
-                  class="tw-font-serif tw-text-2xl tw-font-semibold md:tw-text-3xl lg:tw-text-5xl"
+                  class="tw-pb-8 tw-text-2xl tw-font-semibold md:tw-pb-4 md:tw-text-3xl lg:tw-text-5xl xl:tw-pb-0"
                 >
-                  Empowering the Next Generation of Leaders
+                  Smart Students, Smart Solutions
                 </div>
-                <div class="tw-font-serif tw-text-lg lg:tw-text-2xl">
-                  Application Deadline: March 26, 2023
+                <div class="tw-hidden 2xl:tw-block">
+                  <div class="tw-text-lg tw-font-bold lg:tw-text-2xl">
+                    Student Deferred Loans*
+                  </div>
+                  <div
+                    class="tw-text-base tw-capitalize lg:tw-text-xl"
+                  >
+                    Loans up to $20,000
+                  </div>
                 </div>
-                <div class="tw-mt-4">
+                <div class="tw-hidden 2xl:tw-block">
+                  <div class="tw-text-lg tw-font-bold lg:tw-text-2xl">
+                    Student Consolidated Loans*
+                  </div>
+                  <div
+                    class="tw-text-base tw-capitalize lg:tw-text-xl"
+                  >
+                    Loans up to $80,000*
+                  </div>
+                </div>
+                <div class="tw-hidden 2xl:tw-block">
+                  <div class="tw-text-lg tw-font-bold lg:tw-text-2xl">
+                    Student Checking Accounts
+                  </div>
+                  <div
+                    class="tw-text-base tw-capitalize lg:tw-text-xl"
+                  >
+                    Hassle-free checking accounts with tiered interest
+                    rates and no minimum balance
+                  </div>
+                </div>
+
+                <div class="tw-hidden 4xl:tw-block">
+                  <div
+                    class="tw-text-lg tw-font-semibold lg:tw-text-2xl"
+                  >
+                    Student Credit Cards
+                  </div>
+                  <div
+                    class="tw-text-base tw-capitalize lg:tw-text-xl"
+                  >
+                    Visa<sup>®</sup> credit cards with fixed rates as
+                    low as 9.90% APR*
+                  </div>
+                </div>
+                <div class="button-de tw-pb-8 md:tw-pb-4 xl:tw-pb-0">
                   <a
-                    class="text-shadow-0 tw-inline-flex tw-items-center tw-rounded-md tw-bg-brandingRed-400 tw-px-2 tw-py-2 tw-font-bold tw-capitalize tw-text-white tw-shadow-md tw-transition-all tw-ease-in-out hover:tw-scale-110 md:tw-text-lg"
-                    href="http://"
+                    class="text-shadow-0 tw-inline-flex tw-items-center tw-rounded-md tw-bg-brandingRed-400 tw-px-2 tw-py-2 tw-font-bold tw-capitalize tw-text-white tw-shadow-md tw-transition-all tw-ease-in-out hover:tw-scale-110 hover:tw-text-white md:tw-text-lg"
+                    href="https://www.platinumfcu.org/pages/personal-loans.php"
                     target="_blank"
                     rel="noopener noreferrer"
-                    >apply today</a
+                    >learn more</a
                   >
+                </div>
+                <div class="tw-text-sm tw-italic">
+                  *All loans subject to credit approval. Terms and
+                  conditions apply.
                 </div>
               </div>
             </div>
@@ -199,7 +322,109 @@
               class="overlay tw-absolute tw-z-10 tw-h-full tw-w-full tw-bg-black/75 md:tw-hidden"
             ></div>
             <img
-              src="./images/swiper-banner/scholarship-new.jpg"
+              src="./images/swiper-banner/student-banner.jpg"
+              class="tw-hidden tw-w-full md:tw-block"
+              alt=""
+              srcset=""
+            />
+            <div
+              class="[mobile] tw-absolute tw-flex tw-h-full tw-w-full tw-items-center"
+            >
+              <img
+                src="./images/swiper-banner/student-banner-mobile.jpg"
+                class="tw-h-full tw-w-full tw-object-cover md:tw-hidden"
+                alt=""
+                srcset=""
+              />
+            </div>
+          </div>
+          <!-- 3-in-1 Banner -->
+          <div
+            class="swiper-slide min-h-350 text-shadow tw-bg-brandingGreen-400"
+          >
+            <div class="tw-mx-auto tw-flex tw-max-w-[1500px]">
+              <div
+                class="content tw-absolute tw-top-1/2 tw-z-50 tw-flex tw-w-full tw-max-w-[50rem] tw--translate-y-1/2 tw-flex-col tw-gap-2 tw-px-2 tw-text-center tw-font-serif tw-text-white md:tw-top-1/2 md:tw--translate-y-1/2 md:tw-gap-y-4 md:tw-px-8 md:tw-text-left xl:tw-gap-2 4xl:tw-gap-4"
+              >
+                <div
+                  class="tw-text-2xl tw-font-semibold tw-capitalize md:tw-text-3xl lg:tw-text-4xl xl:tw-text-2xl 2xl:tw-text-4xl"
+                >
+                  Affording life can be hard.
+                  <br class="xl:tw-hidden" />
+                  We're here to help!
+                </div>
+                <div class="tw-hidden tw-text-lg lg:tw-text-xl">
+                  Introducing 3 affordable loans, tailor-made for your
+                  budget
+                </div>
+
+                <div
+                  class="tw-hidden tw-flex-col tw-gap-2 xl:tw-flex xl:tw-gap-0 4xl:tw-gap-2"
+                >
+                  <div class="">
+                    <div
+                      class="tw-text-lg tw-font-bold 2xl:tw-text-2xl 4xl:tw-text-3xl"
+                    >
+                      5-5-5 Loans
+                    </div>
+                    <div
+                      class="tw-text-lg lg:tw-text-lg 4xl:tw-text-xl"
+                    >
+                      Borrow up to $5,000 up to 5 years at 5.00% APR*
+                    </div>
+                  </div>
+                  <div class="">
+                    <div
+                      class="tw-text-lg tw-font-bold 2xl:tw-text-2xl 4xl:tw-text-3xl"
+                    >
+                      VIP (Vocational Income Program) Loans
+                    </div>
+                    <div
+                      class="tw-text-lg tw-italic lg:tw-text-lg 4xl:tw-text-xl"
+                    >
+                      Purpose Vocational Education for Income
+                      Enhancement; School should be accredited by
+                      ACCSC
+                    </div>
+                  </div>
+                  <div class="">
+                    <div
+                      class="tw-text-lg tw-font-bold 2xl:tw-text-2xl 4xl:tw-text-3xl"
+                    >
+                      Small Dollar Auto Loans
+                    </div>
+                    <div
+                      class="tw-text-lg lg:tw-text-lg 4xl:tw-text-xl"
+                    >
+                      Borrow up to $20,000 up to 6 years at low rates
+                    </div>
+                  </div>
+                </div>
+                <div class="tw-mt-8 xl:tw-mt-4">
+                  <a
+                    class="text-shadow-0 tw-inline-flex tw-items-center tw-rounded-md tw-bg-brandingRed-400 tw-px-2 tw-py-2 tw-font-bold tw-capitalize tw-text-white tw-shadow-md tw-transition-all tw-ease-in-out hover:tw-scale-110 hover:tw-text-white md:tw-text-lg"
+                    href="https://app.loanspq.com/apply.aspx?enc=Kw21Wblm1yxpjJabdoZaD3Zx_8IQUk-1-ZZb7V-EuMlNwTb71KD-oThHBH4yCJXnhwl44y9bdO5AH7utGaw1ZFi4CDpdjWy6li7TxQLo3_LLG1XlO_G40zNb3ajpsXX2"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >learn more & apply now!</a
+                  >
+                </div>
+                <div
+                  class="tw-hidden tw-text-xs tw-italic xl:tw-block"
+                >
+                  *Annual Percentage Rate. All loans subject to credit
+                  approval. All loans state above are for members with
+                  income less than $3,000/month. Terms and
+                  restrictions apply. See credit union for full
+                  details.
+                </div>
+              </div>
+            </div>
+            <div
+              class="overlay tw-absolute tw-z-10 tw-h-full tw-w-full tw-bg-black/75 md:tw-hidden"
+            ></div>
+            <img
+              src="./images/swiper-banner/3-in-1-banner.jpg"
               class="tw-hidden md:tw-block"
               alt=""
               srcset=""
@@ -208,7 +433,7 @@
               class="[mobile] tw-absolute tw-flex tw-h-full tw-w-full tw-items-center"
             >
               <img
-                src="./images/swiper-banner/scholarship-new-mobile.jpg"
+                src="./images/swiper-banner/3-in-1-mobile.jpg"
                 class="tw-h-full tw-w-full tw-object-cover md:tw-hidden"
                 alt=""
                 srcset=""
