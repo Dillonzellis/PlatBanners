fd42238fdd22e7d9c122dd65fcc089d971bcbe69
Dillon Ellis <dellis@fcul.com>
2023-04-12 14:32:31 -0400

    update loan forms

--- inner-pages/forms/loan-forms.php
+++ inner-pages/forms/loan-forms.php
@@ -41,6 +41,7 @@
     <link rel="stylesheet" href="../remodal/dist/remodal-default-theme.css">
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-59046801-1"></script>
+<link rel="stylesheet" href="../css/swiper-banners.css">
 </head>
 
 <body>
@@ -53,28 +54,32 @@
     <!-- loading End -->
     <!-- HEADER  -->
     <header class="header-06 white">
-        <?php include '../inc/header.php';?>
+        <?php include '../inc/header.php'; ?>
     </header>
     <!-- /HEADER END -->
     <!-- search -->
     <div class="search hidden">
-        <?php include '../inc/site-search.php';?>
+        <?php include '../inc/site-search.php'; ?>
     </div>
     <!-- /search END -->
     <!--======= Breadcrumb Left With BG Image =======-->
-    <section class="overview-block-ptb1 iq-over-black-10 jarallax iq-breadcrumb3 text-left iq-font-white" style="background-color: #575757;">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-12">
-                    <div class="iq-mb-0">
-                        <h2 class="iq-font-white iq-tw-6 custom-header">Forms</h2>
-                    </div>
+    <section class="tw-mt-[76px] tw-bg-brandingGreen-400">
+        <div class="">
+            <div
+                class="tw-flex tw-max-w-[1920px] tw-items-center tw-justify-center tw-gap-12 lg:tw-min-h-[250px] lg:tw-justify-start">
+                <img src="../images/inner-header/inner-pattern.png" alt=""
+                    class="tw-absolute tw-hidden tw-h-[250px] tw-w-[675px] tw-object-cover lg:tw-block" srcset="" />
+                <div class="tw-mx-auto tw-flex tw-w-full container tw-px-[15px]">
+                    <h1
+                        class="tw-text-shadow tw-md:text-4xl tw-py-8 tw-font-serif tw-text-2xl tw-font-bold tw-text-white lg:tw-text-5xl">
+                        Forms
+                    </h1>
                 </div>
             </div>
         </div>
     </section>
 	<!--HIDDEN NAV-->
-	<?php include '../inc/forms-hidden-nav.php';?>
+	<?php include '../inc/forms-hidden-nav.php'; ?>
     <!--======= Breadcrumb Left With BG Image =======-->
     <!--=================================
 MAIN CONTENT -->
@@ -97,7 +102,7 @@ Tabs -->
                                 <a class="nav-link" href="other-forms.php">Other Forms</a>
                             </li>
                         </ul>
-                        <div class="green-bg iq-mt-30 iq-pall-20">
+                        <div class="red-bg iq-mt-30 iq-pall-20">
                             <h6 class="iq-tw-6 iq-font-black small-title white">How can We Help You?</h6>
                             <div class="iq-font-white">Access your account at PFCU 24hr a day with Home Banking or call us at 1(877)PFCU4YO (732-8496) or call/text us at (404) 297-9797. 
 </div>
@@ -116,7 +121,7 @@ Tabs -->
                         <div class="tab-content">
                             <div class="tab-pane fade show active">
                                 <div class="row">
-									<div class="col-sm-12"><h3 class="iq-pb-25">Loan Forms</h3></div>
+									<!-- <div class="col-sm-12"><h3 class="iq-pb-25">Loan Forms</h3></div> -->
                                     <div class="col-lg-6">
 
 										<h6 class="iq-pbt-15">Auto Loans</h6>
@@ -245,7 +250,7 @@ Tabs -->
             </div>
         </div>
 		<!--HIDDEN GREEN-->
-		<?php include '../inc/hidden-green.php';?>
+		<?php include '../inc/hidden-green.php'; ?>
         <!--=================================
 Tabs -->
     </div>
@@ -254,7 +259,7 @@ Main Content -->
     <!--=================================
 Footer -->
     <footer class="iq-footer6">
-        <?php include '../inc/footer.php';?>
+        <?php include '../inc/footer.php'; ?>
     </footer>
     <!--=================================
 Footer -->
@@ -264,8 +269,283 @@ Footer -->
         <a class="top" id="top" href="#top" aria-label="button to return to top of page"><img src="" alt="Image to go to top of page" style="display:none;"> <i class="ion-ios-arrow-up"></i> </a>
     </div>
     <!-- back-to-top End -->
-	<?php include '../inc/modals.php';?>
-    <?php include '../inc/inner-scripts.php';?>
+	<?php include '../inc/modals.php'; ?>
+    <?php include '../inc/inner-scripts.php'; ?>
+</body>
+
+</html><!doctype html>
+<html lang="en">
+
+<head>
+    <title>Platinum FCU - Loan Forms</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../images/favicon.ico" />
+    <!-- bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- font awesome -->
+    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- ionicons icon -->
+    <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- mega menu -->
+    <link href="../css/mega-menu/mega_menu.css" rel="stylesheet" type="text/css" />
+    <!-- owl-carousel -->
+    <link href="../css/owl-carousel/owl.carousel.css" rel="stylesheet" type="text/css" />
+    <!-- magnific popup -->
+    <link href="../css/magnific-popup.css" rel="stylesheet" type="text/css" />
+    <!-- animate -->
+    <link href="../css/animate.css" rel="stylesheet" type="text/css" />
+    <!-- media element player -->
+    <link href="../css/mediaelementplayer.min.css" rel="stylesheet" type="text/css" />
+    <!-- REVOLUTION STYLE SHEETS -->
+    <link href="../revolution/css/settings.css" rel="stylesheet" type="text/css">
+    <!-- ADD-ONS CSS FILES -->
+    <link href="../revolution/css/revolution.addon.particles.css" rel="stylesheet" type="text/css">
+    <!-- shortcodes -->
+    <link href="../css/shortcodes.css" rel="stylesheet" type="text/css" />
+    <!-- main style -->
+    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+    <!-- responsive -->
+    <link href="../css/responsive.css" rel="stylesheet" type="text/css" />
+    <!-- custom -->
+    <link href="../css/custom.css" rel="stylesheet" type="text/css" />
+	<!-- Remodal styles -->
+    <link rel="stylesheet" href="../remodal/dist/remodal.css">
+    <link rel="stylesheet" href="../remodal/dist/remodal-default-theme.css">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-59046801-1"></script>
+<link rel="stylesheet" href="../css/swiper-banners.css">
+</head>
+
+<body>
+    <!-- loading -->
+    <div id="loading">
+        <div id="loading-center">
+            <img src="../images/loader.png" alt="loder">
+        </div>
+    </div>
+    <!-- loading End -->
+    <!-- HEADER  -->
+    <header class="header-06 white">
+        <?php include '../inc/header.php'; ?>
+    </header>
+    <!-- /HEADER END -->
+    <!-- search -->
+    <div class="search hidden">
+        <?php include '../inc/site-search.php'; ?>
+    </div>
+    <!-- /search END -->
+    <!--======= Breadcrumb Left With BG Image =======-->
+    <section class="tw-mt-[76px] tw-bg-brandingGreen-400">
+        <div class="">
+            <div
+                class="tw-flex tw-max-w-[1920px] tw-items-center tw-justify-center tw-gap-12 lg:tw-min-h-[250px] lg:tw-justify-start">
+                <img src="../images/inner-header/inner-pattern.png" alt=""
+                    class="tw-absolute tw-hidden tw-h-[250px] tw-w-[675px] tw-object-cover lg:tw-block" srcset="" />
+                <div class="tw-mx-auto tw-flex tw-w-full container tw-px-[15px]">
+                    <h1
+                        class="tw-text-shadow tw-md:text-4xl tw-py-8 tw-font-serif tw-text-2xl tw-font-bold tw-text-white lg:tw-text-5xl">
+                        Forms
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </section>
+	<!--HIDDEN NAV-->
+	<?php include '../inc/forms-hidden-nav.php'; ?>
+    <!--======= Breadcrumb Left With BG Image =======-->
+    <!--=================================
+MAIN CONTENT -->
+    <div class="main-content">
+        <!--=================================
+Tabs -->
+        <div class="overview-block-ptb iq-tab6">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-4 col-sm-12 iq-mtb-15">
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-pills tabs-left" id="pills-tab" >
+                            <li class="nav-item">
+                                <a class="nav-link active" href="loan-forms.php">Loan Forms</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="membership-forms.php">Membership Forms</a>
+                            </li>
+							<li class="nav-item">
+                                <a class="nav-link" href="other-forms.php">Other Forms</a>
+                            </li>
+                        </ul>
+                        <div class="red-bg iq-mt-30 iq-pall-20">
+                            <h6 class="iq-tw-6 iq-font-black small-title white">How can We Help You?</h6>
+                            <div class="iq-font-white">Access your account at PFCU 24hr a day with Home Banking or call us at 1(877)PFCU4YO (732-8496) or call/text us at (404) 297-9797. 
+</div>
+                            <div class="iq-mt-20 read-more white green">
+                                <a href="tel:+18777328496"><img src="../images/about-us/icon.png" alt="">
+<span class="iq-font-yellow iq-pl-10 iq-tw-6 iq-lead"> +1877 732 8496</span><br>
+</a>
+                            <a href="tel:+14042979797"><div class="coolBox"></div>
+<span class="iq-font-yellow iq-pl-10 iq-tw-6 iq-lead"> +1404 297 9797</span><br>
+</a>
+</div>
+                        </div>
+                    </div>
+                    <div class="col-lg-9 col-md-8 col-sm-12 iq-mtb-15">
+                        <!-- Tab panes -->
+                        <div class="tab-content">
+                            <div class="tab-pane fade show active">
+                                <div class="row">
+									<!-- <div class="col-sm-12"><h3 class="iq-pb-25">Loan Forms</h3></div> -->
+                                    <div class="col-lg-6">
+
+										<h6 class="iq-pbt-15">Auto Loans</h6>
+										<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-25">
+											<li><a href="https://www.platinumfcu.org/forms/pdfs/AutoLoanPackage.pdf" target="_blank">Auto Loan Application Package</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/verification-employment.pdf" target="_blank">Telephone Verification of Employment</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/guarantors-profile.pdf" target="_blank">Guarantor Profile</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/AutoMV-1.pdf" target="_blank">Motor Vehicle Services and/or County Tag Agent Title/Tag Application</a></li>
+										</ul>
+										
+										<p>&nbsp;</p>
+										
+										<h6 class="iq-pbt-15">Commercial Real Estate Loan</h6>
+										<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-25">
+											<li><a href="https://www.platinumfcu.org/forms/business-loan-app-RE.pdf" target="_blank">Application Form</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/business-guaranty-agreement.pdf" target="_blank">Business Guaranty Agreement</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/resolution-authority.pdf" target="_blank">Business Resolution</a></li>
+										</ul>
+										
+										<p>&nbsp;</p>
+										
+										<h6 class="iq-pbt-15">Home Equity Loan</h6>
+										<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-25">
+											<li><a href="https://www.platinumfcu.org/forms/mortgage-loan-app.pdf" target="_blank">Application Form</a></li>
+										</ul>
+										
+										<p>&nbsp;</p>
+										
+										<h6 class="iq-pbt-15">Home Mortgage Loan</h6>
+										<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-25">
+											<li><a href="https://www.platinumfcu.org/forms/mortgage-loan-app.pdf" target="_blank">Application Form</a></li>
+											<li><a href="#" data-remodal-target="mortgage">Online Mortgage Pre-Qualification Application</a></li>
+										</ul>
+										
+										<p>&nbsp;</p>
+										
+										<h6 class="iq-pbt-15">Signature Loan</h6>
+										<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-25">
+											<li><a href="https://www.platinumfcu.org/forms/loan.pdf" target="_blank">Application Form</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/signature-loan-sheet.pdf" target="_blank">Signature Loans Sheet</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/financial-statement.pdf" target="_blank">Personal Financial Statement</a></li>
+										</ul>
+										
+										<p>&nbsp;</p>
+										
+										<h6 class="iq-pbt-15">Student Deferred Loan</h6>
+										<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-25">
+											<li><a href="https://www.platinumfcu.org/forms/oeloan.pdf" target="_blank">Application Form</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/verification-employment.pdf" target="_blank">Telephone Verification of Employment</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/financial-statement.pdf" target="_blank">Personal Financial Statement</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/guarantors-profile.pdf" target="_blank">Guarantor Profile</a></li>
+										</ul>
+										
+										<p>&nbsp;</p>
+										
+										<h6 class="iq-pbt-15">Student Loan</h6>
+										<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-25">
+											<li><a href="https://www.platinumfcu.org/forms/loan.pdf" target="_blank">Application Form</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/verification-employment.pdf" target="_blank">Telephone Verification of Employment</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/financial-statement.pdf" target="_blank">Personal Financial Statement</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/guarantors-profile.pdf" target="_blank">Guarantor Profile (x2)</a></li>
+										</ul>
+										
+                                    </div>
+									
+									<div class="col-lg-6">
+										
+										<h6 class="iq-pbt-15">Business MasterCard&reg; Credit Cards</h6>
+										<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-15">
+											<li><a href="https://www.platinumfcu.org/forms/Business-CC-Application.pdf" target="_blank">Business Expanded Application</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/Business-CC-Addendum-Agreement.pdf" target="_blank">Business Credit Card Addendum &amp; Agreement</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/resolution-authority.pdf" target="_blank">Business Resolution</a></li>
+										</ul>
+										
+										<p>&nbsp;</p>
+										
+										<h6 class="iq-pbt-15">Personal VISA&reg; Platinum Credit Cards</h6>
+										<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-15">
+											<li><a href="https://www.platinumfcu.org/forms/cc-application.pdf" target="_blank">Credit Card Application Form</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/cc-agreement.pdf" target="_blank">Credit Card Agreement</a></li>
+										</ul>
+										
+										<p>&nbsp;</p>
+										
+										<h6 class="iq-pbt-15">Member Business Loan</h6>
+										<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-25">
+											<li><a href="https://www.platinumfcu.org/forms/business-loan-app.pdf" target="_blank">Application Form</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/Business-Loan-Agreement.pdf" target="_blank">Business Loan Agreement</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/business-guaranty-agreement.pdf" target="_blank">Business Guaranty Agreement</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/consent-of-landlord.pdf" target="_blank">Consent of Landlord</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/4506T.pdf" target="_blank">4506-T</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/resolution-authority.pdf" target="_blank">Business Resolution</a></li>
+										</ul>
+										
+										<p>&nbsp;</p>
+										
+										<h6 class="iq-pbt-15">Emergency Loan</h6>
+										<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-25">
+											<li><a href="https://www.platinumfcu.org/forms/loan.pdf" target="_blank">Application Form</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/verification-employment.pdf" target="_blank">Telephone Verification of Employment</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/financial-statement.pdf" target="_blank">Personal Financial Statement</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/guarantors-profile.pdf" target="_blank">Guarantor Profile</a></li>
+										</ul>
+										
+										<p>&nbsp;</p>
+										
+										<h6 class="iq-pbt-15">Personal Loan</h6>
+										<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-25">
+											<li><a href="https://www.platinumfcu.org/forms/loan.pdf" target="_blank">Application Form</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/personal-Loan-sheet.pdf" target="_blank">Personal Loan Sheet</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/verification-employment.pdf" target="_blank">Telephone Verification of Employment</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/financial-statement.pdf" target="_blank">Personal Financial Statement</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/financial.pdf" target="_blank">Personal Financial Statement (Loans $25,000 & above)</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/4506T.pdf" target="_blank">4506-T</a></li>
+											<li><a href="https://www.platinumfcu.org/forms/guarantors-profile.pdf" target="_blank">Guarantor Profile (x2)</a></li>
+										</ul>
+										
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+		<!--HIDDEN GREEN-->
+		<?php include '../inc/hidden-green.php'; ?>
+        <!--=================================
+Tabs -->
+    </div>
+    <!--=================================
+Main Content -->
+    <!--=================================
+Footer -->
+    <footer class="iq-footer6">
+        <?php include '../inc/footer.php'; ?>
+    </footer>
+    <!--=================================
+Footer -->
+
+    <!-- back-to-top -->
+    <div id="back-to-top">
+        <a class="top" id="top" href="#top" aria-label="button to return to top of page"><img src="" alt="Image to go to top of page" style="display:none;"> <i class="ion-ios-arrow-up"></i> </a>
+    </div>
+    <!-- back-to-top End -->
+	<?php include '../inc/modals.php'; ?>
+    <?php include '../inc/inner-scripts.php'; ?>
 </body>
 
 </html>
