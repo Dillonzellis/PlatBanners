04ecb3d33aa5fecd511f62471d8b4032f9e17901
Dillon Ellis <dellis@fcul.com>
2023-04-12 12:04:25 -0400

    business dep and deposit products page

--- inner-pages/deposit-products.php
+++ inner-pages/deposit-products.php
@@ -37,15 +37,10 @@
     <!-- custom -->
     <link href="../css/custom.css" rel="stylesheet" type="text/css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
-      rel="preconnect"
-      href="https://fonts.gstatic.com"
-      crossorigin
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-      rel="stylesheet"
-    />
+        href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet" />
     <link rel="stylesheet" href="../css/swiper-banners.css">
 </head>
 
@@ -68,32 +63,24 @@
     </div>
     <!-- /search END -->
     <section class="tw-mt-[76px] tw-bg-brandingGreen-400">
-      <div class="">
-        <div
-          class="tw-flex tw-max-w-[1920px] tw-items-center tw-justify-center tw-gap-12 lg:tw-min-h-[250px] lg:tw-justify-start"
-        >
-          <img
-            src="../images/inner-header/inner-pattern.png"
-            alt=""
-            class="tw-absolute tw-hidden tw-h-[250px] tw-w-[675px] tw-object-cover lg:tw-block"
-            srcset=""
-          />
-          <div
-            class="tw-mx-auto tw-flex tw-w-full container tw-px-[15px]"
-          >
-  
-              <h1
-                class="tw-text-shadow tw-md:text-4xl tw-py-8 tw-font-serif tw-text-2xl tw-font-bold tw-text-white lg:tw-text-5xl"
-              >
-                Deposit Products
-              </h1>
-      
-          </div>
+        <div class="">
+            <div
+                class="tw-flex tw-max-w-[1920px] tw-items-center tw-justify-center tw-gap-12 lg:tw-min-h-[250px] lg:tw-justify-start">
+                <img src="../images/inner-header/inner-pattern.png" alt=""
+                    class="tw-absolute tw-hidden tw-h-[250px] tw-w-[675px] tw-object-cover lg:tw-block" srcset="" />
+                <div class="tw-mx-auto tw-flex tw-w-full container tw-px-[15px]">
+
+                    <h1
+                        class="tw-text-shadow tw-md:text-4xl tw-py-8 tw-font-serif tw-text-2xl tw-font-bold tw-text-white lg:tw-text-5xl">
+                        Deposit Products
+                    </h1>
+
+                </div>
+            </div>
         </div>
-      </div>
     </section>
-	<!--HIDDEN NAV-->
-	<?php include '../inc/deposit-hidden-nav.php';?>
+    <!--HIDDEN NAV-->
+    <?php include '../inc/deposit-hidden-nav.php';?>
     <!--======= Breadcrumb Left With BG Image =======-->
     <!--=================================
 MAIN CONTENT -->
@@ -105,26 +92,29 @@ Tabs -->
                 <div class="row flex-col-reverse-max">
                     <div class="col-lg-3 col-md-4 col-sm-12 iq-mtb-15">
                         <!-- Nav tabs -->
-                        <ul class="nav nav-pills tabs-left" id="pills-tab" >
-							<li class="nav-item">
-								<a class="nav-link" href="business-deposit.php">Business Deposits</a>
-							</li>
+                        <ul class="nav nav-pills tabs-left" id="pills-tab">
                             <li class="nav-item">
-								<a class="nav-link" href="personal-deposit.php">Personal Deposits</a>
-							</li>
+                                <a class="nav-link" href="business-deposit.php">Business Deposits</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="personal-deposit.php">Personal Deposits</a>
+                            </li>
                         </ul>
-                          <div class="iq-mt-30 iq-pall-20" style="background-color:#b2191e"">
-                            <h6 style="color:white" class="iq-tw-6 iq-font-black small-title white">How can We Help You?</h6>
-                            <div class="iq-font-white">Access your account at PFCU 24hr a day with Home Banking or call us at 1(877)PFCU4YO (732-8496) or call/text us at (404) 297-9797. 
-</div>
+                        <div class="iq-mt-30 iq-pall-20" style="background-color:#b2191e"">
+                            <h6 style=" color:white" class="iq-tw-6 iq-font-black small-title white">How can We Help
+                            You?</h6>
+                            <div class="iq-font-white">Access your account at PFCU 24hr a day with Home Banking or call
+                                us at 1(877)PFCU4YO (732-8496) or call/text us at (404) 297-9797.
+                            </div>
                             <div class="iq-mt-20 read-more white green">
                                 <a href="tel:+18777328496"><img src="../images/about-us/icon.png" alt="">
-<span class="iq-font-yellow iq-pl-10 iq-tw-6 iq-lead"> +1877 732 8496</span><br>
-</a>
-                            <a href="tel:+14042979797"><div class="coolBox"></div>
-<span class="iq-font-yellow iq-pl-10 iq-tw-6 iq-lead"> +1404 297 9797</span><br>
-</a>
-</div>
+                                    <span class="iq-font-yellow iq-pl-10 iq-tw-6 iq-lead"> +1877 732 8496</span><br>
+                                </a>
+                                <a href="tel:+14042979797">
+                                    <div class="coolBox"></div>
+                                    <span class="iq-font-yellow iq-pl-10 iq-tw-6 iq-lead"> +1404 297 9797</span><br>
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <div class="col-lg-9 col-md-8 col-sm-12 iq-mtb-15">
@@ -133,7 +123,14 @@ Tabs -->
                             <div class="tab-pane fade show active">
                                 <div class="row">
                                     <div class="col-sm-12">
-										<p>Our community is known to keep the business within the community. Our community trade associations and the credit unions are a great example of this principle. That's why at PFCU you’ll find the products and services to meet your business banking needs – from checking and mobile banking for managing your day-to-day finances to a complete suite of complex cash management needs. When you choose PFCU, you’ve got a banking partner who knows a thing or two about doing business within the community. Together we continue to grow stronger.</p>
+                                        <p>Our community is known to keep the business within the community. Our
+                                            community trade associations and the credit unions are a great example of
+                                            this principle. That's why at PFCU you’ll find the products and services to
+                                            meet your business banking needs – from checking and mobile banking for
+                                            managing your day-to-day finances to a complete suite of complex cash
+                                            management needs. When you choose PFCU, you’ve got a banking partner who
+                                            knows a thing or two about doing business within the community. Together we
+                                            continue to grow stronger.</p>
                                     </div>
                                 </div>
                             </div>
@@ -144,8 +141,8 @@ Tabs -->
                 </div>
             </div>
         </div>
-		<!--HIDDEN GREEN-->
-		<?php include '../inc/hidden-green.php';?>
+        <!--HIDDEN GREEN-->
+        <?php include '../inc/hidden-green.php';?>
         <!--=================================
 Tabs -->
     </div>
@@ -161,7 +158,8 @@ Footer -->
 
     <!-- back-to-top -->
     <div id="back-to-top">
-        <a class="top" id="top" href="#top" aria-label="button to return to top of page"><img src="" alt="Image to go to top of page" style="display:none;"> <i class="ion-ios-arrow-up"></i> </a>
+        <a class="top" id="top" href="#top" aria-label="button to return to top of page"><img src=""
+                alt="Image to go to top of page" style="display:none;"> <i class="ion-ios-arrow-up"></i> </a>
     </div>
     <!-- back-to-top End -->
     <?php include '../inc/inner-scripts.php';?>
