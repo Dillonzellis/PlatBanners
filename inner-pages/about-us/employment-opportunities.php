c65726e69e4426959af2e24e2981079ddc457634
Dillon Ellis <dellis@fcul.com>
2023-04-12 15:00:32 -0400

    og about files

---
+++ inner-pages/about-us/employment-opportunities.php
@@ -0,0 +1,245 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Platinum FCU - Employment Opportunities</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../images/favicon.ico" />
+    <!-- bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- font awesome -->
+    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- ionicons icon -->
+    <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- mega menu -->
+    <link href="../css/mega-menu/mega_menu.css" rel="stylesheet" type="text/css" />
+    <!-- owl-carousel -->
+    <link href="../css/owl-carousel/owl.carousel.css" rel="stylesheet" type="text/css" />
+    <!-- magnific popup -->
+    <link href="../css/magnific-popup.css" rel="stylesheet" type="text/css" />
+    <!-- animate -->
+    <link href="../css/animate.css" rel="stylesheet" type="text/css" />
+    <!-- media element player -->
+    <link href="../css/mediaelementplayer.min.css" rel="stylesheet" type="text/css" />
+    <!-- REVOLUTION STYLE SHEETS -->
+    <link href="../revolution/css/settings.css" rel="stylesheet" type="text/css">
+    <!-- ADD-ONS CSS FILES -->
+    <link href="../revolution/css/revolution.addon.particles.css" rel="stylesheet" type="text/css">
+    <!-- shortcodes -->
+    <link href="../css/shortcodes.css" rel="stylesheet" type="text/css" />
+    <!-- main style -->
+    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+    <!-- responsive -->
+    <link href="../css/responsive.css" rel="stylesheet" type="text/css" />
+    <!-- custom -->
+    <link href="../css/custom.css" rel="stylesheet" type="text/css" />
+	<!-- Remodal styles -->
+    <link rel="stylesheet" href="../remodal/dist/remodal.css">
+    <link rel="stylesheet" href="../remodal/dist/remodal-default-theme.css">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-59046801-1"></script>
+</head>
+
+<body>
+    <!-- loading -->
+    <div id="loading">
+        <div id="loading-center">
+            <img src="../images/loader.png" alt="loder">
+        </div>
+    </div>
+    <!-- loading End -->
+    <!-- HEADER  -->
+    <header class="header-06 white">
+        <?php include '../inc/header.php';?>
+    </header>
+    <!-- /HEADER END -->
+    <!-- search -->
+    <div class="search hidden">
+        <?php include '../inc/site-search.php';?>
+    </div>
+    <!-- /search END -->
+    <!--======= Breadcrumb Left With BG Image =======-->
+    <section class="overview-block-ptb1 iq-over-black-10 jarallax iq-breadcrumb3 text-left iq-font-white" style="background-color: #575757;">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <div class="iq-mb-0">
+                        <h2 class="iq-font-white iq-tw-6 custom-header">Employment Opportunities</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+	<!---------------HIDDEN NAVIGATION----------------->
+			<?php include '../inc/about-hidden-nav.php';?>
+	<!---------------HIDDEN NAVIGATION----------------->
+
+    <!--======= Breadcrumb Left With BG Image =======-->
+    <!--=================================
+MAIN CONTENT -->
+    <div class="main-content">
+        <!--=================================
+Tabs -->
+        <div class="overview-block-ptb iq-tab6">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-4 col-sm-12 iq-mtb-15">
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-pills tabs-left" id="pills-tab" >
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact-us.php">Contact Us</a>
+                            </li>
+  <li class="nav-item">
+                                <a class="nav-link" href="who-we-are.php">Who We Are</a>
+                            </li>
+  <li class="nav-item">
+                                <a class="nav-link" href="board.php">Board of Directors</a>
+                            </li>
+							  <li class="nav-item">
+                                <a class="nav-link active" href="employment-opportunities.php">Employment Opportunities</a>
+                            </li>
+							<li class="nav-item">
+                                <a class="nav-link" href="faqs.php">FAQ's</a>
+                            </li>
+							<li class="nav-item">
+                                <a class="nav-link" href="news.php">News & Resources</a>
+                            </li>
+							<li class="nav-item">
+                                <a class="nav-link" href="../pdf/PrivacyPolicy.pdf" target="_blank">Privacy Policy</a>
+                            </li>
+							<li class="nav-item">
+                                <a class="nav-link" href="fraud-protection.php">Fraud Protection</a>
+                            </li>
+							<li class="nav-item">
+                                <a class="nav-link" href="calendar.php">Calendar</a>
+                            </li>
+                        </ul>
+                         <div class="green-bg iq-mt-30 iq-pall-20">
+                            <h6 class="iq-tw-6 iq-font-black small-title white">How can We Help You?</h6>
+                            <div class="iq-font-white">Access your account at PFCU 24hr a day with Home Banking or call us at 1(877)PFCU4YO (732-8496) or call/text us at (404) 297-9797. 
+</div>
+                            <div class="iq-mt-20 read-more white green">
+                                <a href="tel:+18777328496"><img src="../images/about-us/icon.png" alt="">
+<span class="iq-font-yellow iq-pl-10 iq-tw-6 iq-lead"> +1877 732 8496</span><br>
+</a>
+                            <a href="tel:+14042979797"><div class="coolBox"></div>
+<span class="iq-font-yellow iq-pl-10 iq-tw-6 iq-lead"> +1404 297 9797</span><br>
+</a>
+</div>
+                        </div>
+                    </div>
+					
+					<div class="col-lg-9 col-md-8 col-sm-12 iq-mtb-15">
+                        <!-- Tab 1 -->
+        <div class="event iq-hide">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 tab-border iq-accordion">
+						<h3 class="iq-pb-25">Open Positions</h3>
+                        <div class="tab">
+                            <!-- Tab panes -->
+                            <div class="tab-content">
+                                <div class="tab-pane active">
+                                    <div id="accordion">
+										<div class="card">
+										<div class="card-header" id="headingTwo">
+											<h5 class="mb-0">
+											<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">TELLER / CASHIER
+											</button>
+										</h5>
+										</div>
+										<div id="collapseTwo" class="collapse" data-parent="#accordion">
+											<div class="card-body">
+												<p><strong><em>Reports to: Head Teller</em></strong></p>
+												<p><strong><em>Duties:</em></strong></p>
+												<p>Provide  basic cash receipt and payment services in accordance with credit union  procedures and in compliance with Bank Secrecy Act.  </p>
+												
+											
+												
+												<p><strong>Duties and Responsibilities:</strong></p>
+												<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-15">
+													<li>Receive share drafts/checks and cash for deposit to accounts, verify amounts, examine share drafts/checks for proper endorsement, and enter deposits into computer records.</li>
+													<li>Cash share drafts/checks and process withdrawals; pay out money after verification of signatures and member balances.</li>
+													<li>Promote and explain other credit union services such as consumer and mortgage loans, IRAs, certificates, safe deposit boxes </li>
+													<li>Identify customer needs, provide information on new products and services, and direct customers to branch representatives as needed. Receive Mortgage, consumer loan, and other payments and ensure the payments match balances due. Enter payments into computer. Generate member receipts. </li>
+													<li>Place hold on accounts for uncollected funds. </li>
+													<li>Count, check, and package coins and currency.</li>
+													<li>Balance cash drawer at the end of the shift and compare totaled amounts to computer-generated proof sheet. Report any discrepancies to the supervisor as necessary.</li>
+													<li>Ensure that the teller station is properly stocked with forms, supplies, etc.</li>
+													<li>Report malfunctions of teller terminals and other equipment used at the teller station.</li>
+													<li>Check night depository bags and record proper information on the credit union’s forms.</li>
+													<li>Responsible to record CTR form for transactions exceeding  in cash to be in compliance with BSA.</li>
+													<li>Monitor member transactions for any suspicious activity in order to file SAR form.</li>
+													<li>Maintain Cashiers and bankers Check inventory and monitor the required information at the time of sale.</li>
+
+												</ul>
+												
+												<p><strong>Qualifications:</strong></p>
+												<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-15">
+													<li>High school diploma, GED. Prior experience as a Bank/Credit Union Preferred. Should possess basic computer knowledge. General math skills. Good verbal and written communication skills. Knowledge of Hindi & Gujarati language preferred.</li>
+													
+													
+
+
+											  </ul>
+												
+												<p><strong>Benefits</strong></p>
+												<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-15">
+													<li>Compensation Annual salary commensurate with skills and experience. </li>
+													<li>Platinum Federal Credit Union offers an attractive benefits package, including 401k, health, life, dental, vision, and PTO.</li>
+												</ul>
+												
+												<p><strong>Application Process</strong></p>
+												<ul class="listing-mark iq-tw-6 iq-font-black iq-pb-15">
+													<li>A slate of successful candidates will be invited for interviews. Due to the large number of resumes generated, only successful candidates will be contacted. Phone calls are discouraged. Email Resume to <a href="mailto:jobs@platinumfcu.org">Jobs@platinumfcu.org</a></li>
+													<li>Platinum Federal Credit Union is proud to be an equal opportunity employer.</li>
+
+												</ul>
+											</div>
+										</div>
+									</div>
+										
+											
+								</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Tab 1 -->
+                    </div>
+
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+		<!--HIDDEN GREEN-->
+		<?php include '../inc/hidden-green.php';?>
+        <!--=================================
+Tabs -->
+    </div>
+    <!--=================================
+Main Content -->
+    <!--=================================
+Footer -->
+    <footer class="iq-footer6">
+        <?php include '../inc/footer.php';?>
+    </footer>
+    <!--=================================
+Footer -->
+
+    <!-- back-to-top -->
+    <div id="back-to-top">
+        <a class="top" id="top" href="#top" aria-label="button to return to top of page"><img src="" alt="Image to go to top of page" style="display:none;"> <i class="ion-ios-arrow-up"></i> </a>
+    </div>
+    <!-- back-to-top End -->
+	<?php include '../inc/modals.php';?>
+    <?php include '../inc/inner-scripts.php';?>
+</body>
+
+</html>
