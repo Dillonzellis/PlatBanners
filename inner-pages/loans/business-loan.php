4fbabc35daf54852c846300b2f00cade69285e2a
Dillon Ellis <dellis@fcul.com>
2023-04-12 13:53:53 -0400

    original personal loan page

--- inner-pages/loans/business-loan.php
+++ inner-pages/loans/business-loan.php
@@ -41,6 +41,7 @@
     <link rel="stylesheet" href="../remodal/dist/remodal-default-theme.css">
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-59046801-1"></script>
+    <link rel="stylesheet" href="../css/swiper-banners.css">
 </head>
 
 
@@ -64,14 +65,17 @@
     </div>
     <!-- /search END -->
     <!--======= Breadcrumb Left With BG Image =======-->
-    <section class="overview-block-ptb1 iq-over-black-10 jarallax iq-breadcrumb3 text-left iq-font-white"
-        style="background-color: #575757;">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-12">
-                    <div class="iq-mb-0">
-                        <h2 class="iq-font-white iq-tw-6 custom-header">Business Loans</h2>
-                    </div>
+    <section class="tw-mt-[76px] tw-bg-brandingGreen-400">
+        <div class="">
+            <div
+                class="tw-flex tw-max-w-[1920px] tw-items-center tw-justify-center tw-gap-12 lg:tw-min-h-[250px] lg:tw-justify-start">
+                <img src="../images/inner-header/inner-pattern.png" alt=""
+                    class="tw-absolute tw-hidden tw-h-[250px] tw-w-[675px] tw-object-cover lg:tw-block" srcset="" />
+                <div class="tw-mx-auto tw-flex tw-w-full container tw-px-[15px]">
+                    <h1
+                        class="tw-text-shadow tw-md:text-4xl tw-py-8 tw-font-serif tw-text-2xl tw-font-bold tw-text-white lg:tw-text-5xl">
+                        Business Loans
+                    </h1>
                 </div>
             </div>
         </div>
@@ -100,7 +104,7 @@ Tabs -->
                             </li>
 
                         </ul>
-                        <div class="green-bg iq-mt-30 iq-pall-20">
+                        <div class="red-bg iq-mt-30 iq-pall-20">
                             <h6 class="iq-tw-6 iq-font-black small-title white">How can We Help You?</h6>
                             <div class="iq-font-white">Access your account at PFCU 24hr a day with Home Banking or call
                                 us at 1(877)PFCU4YO (732-8496) or call/text us at (404) 297-9797.
@@ -128,11 +132,11 @@ Tabs -->
                                             <!-- Tab panes -->
                                             <div class="tab-content">
                                                 <div class="tab-pane active">
-                                                    <div class="row">
+                                                    <!-- <div class="row">
                                                         <div class="col-sm-12">
                                                             <h3 class="iq-pb-25">Business</h3>
                                                         </div>
-                                                    </div>
+                                                    </div> -->
                                                     <div id="accordion">
                                                         <div class="card">
                                                             <div class="card-header" id="headingOne">
@@ -162,7 +166,8 @@ Tabs -->
                                                                                 homes, convenience stores, gas stations
                                                                                 and retail establishments is available
                                                                                 in loan amounts of up to
-                                                                                <strong>$900,000</strong>.</p>
+                                                                                <strong>$900,000</strong>.
+                                                                            </p>
                                                                             <p>PFCU also partners with Credit Union
                                                                                 Business Services (CUBS) a Credit Union
                                                                                 Service Organization (CUSO) for
@@ -172,7 +177,8 @@ Tabs -->
                                                                                 underwriting, and servicing business
                                                                                 loans, commercial mortgage loans, and
                                                                                 SBA 504 loans for real estate investors
-                                                                                and business owners.</p>
+                                                                                and business owners.
+                                                                            </p>
                                                                             <p><a href="loan-forms.php">Click here for
                                                                                     forms & requirement details</a>. For
                                                                                 more information, contact Chief Lending
