d6f93e53864e75b02d13acd15287072fba4491a5
Dillon Ellis <dellis@fcul.com>
2023-04-12 16:14:09 -0400

    updated contact us

--- src/inner-pages/about-us/contact-us.php
+++ src/inner-pages/about-us/contact-us.php
@@ -1,45 +1,14 @@
 <!doctype html>
 <html lang="en"><head>
-    <title>Platinum FCU - Contact Us</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="../images/favicon.ico" />
-    <!-- bootstrap -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <!-- font awesome -->
-    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <!-- ionicons icon -->
-    <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
-    <!-- mega menu -->
-    <link href="../css/mega-menu/mega_menu.css" rel="stylesheet" type="text/css" />
-    <!-- owl-carousel -->
-    <link href="../css/owl-carousel/owl.carousel.css" rel="stylesheet" type="text/css" />
-    <!-- magnific popup -->
-    <link href="../css/magnific-popup.css" rel="stylesheet" type="text/css" />
-    <!-- animate -->
-    <link href="../css/animate.css" rel="stylesheet" type="text/css" />
-    <!-- media element player -->
-    <link href="../css/mediaelementplayer.min.css" rel="stylesheet" type="text/css" />
-    <!-- REVOLUTION STYLE SHEETS -->
-    <link href="../revolution/css/settings.css" rel="stylesheet" type="text/css">
-    <!-- ADD-ONS CSS FILES -->
-    <link href="../revolution/css/revolution.addon.particles.css" rel="stylesheet" type="text/css">
-    <!-- shortcodes -->
-    <link href="../css/shortcodes.css" rel="stylesheet" type="text/css" />
-    <!-- main style -->
-    <link href="../css/style.css" rel="stylesheet" type="text/css" />
-    <!-- responsive -->
-    <link href="../css/responsive.css" rel="stylesheet" type="text/css" />
-    <!-- custom -->
-    <link href="../css/custom.css" rel="stylesheet" type="text/css" />
-	<!-- Remodal styles -->
-    <link rel="stylesheet" href="../remodal/dist/remodal.css">
-    <link rel="stylesheet" href="../remodal/dist/remodal-default-theme.css">
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-59046801-1"></script>
-</head>
+  
+<?php
+$current = "Contact Us"
+?>
+
+<?php
+$title = $current;
+include '../inc/head.php';
+?>
 
 <body>
     <!-- loading -->
@@ -60,17 +29,10 @@
     </div>
     <!-- /search END -->
     <!--======= Breadcrumb Left With BG Image =======-->
-    <section class="overview-block-ptb1 iq-over-black-10 jarallax iq-breadcrumb3 text-left iq-font-white" style="background-color: #575757;">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-12">
-                    <div class="iq-mb-0">
-                        <h2 class="iq-font-white iq-tw-6 custom-header">About Us</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php
+$page_title = $current;
+include '../inc/about-hero.php';
+?>
 	<!---------------HIDDEN NAVIGATION----------------->
 			<?php include '../inc/about-hidden-nav.php';?>
 	<!---------------HIDDEN NAVIGATION----------------->
@@ -115,7 +77,7 @@ Tabs -->
                                 <a class="nav-link" href="calendar.php">Calendar</a>
                             </li>
                         </ul>
-                        <div class="green-bg iq-mt-30 iq-pall-20">
+                        <div class="red-bg iq-mt-30 iq-pall-20">
                             <h6 class="iq-tw-6 iq-font-black small-title white">How can We Help You?</h6>
                             <div class="iq-font-white">Access your account at PFCU 24hr a day with Home Banking or call us at 1(877)PFCU4YO (732-8496) or call/text us at (404) 297-9797. 
 </div>
