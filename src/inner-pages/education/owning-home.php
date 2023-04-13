ba5751c16d0ae4b69ff2e2acd00fbed2e9e7e500
Dillon Ellis <dellis@fcul.com>
2023-04-13 11:39:09 -0400

    own a home page update

--- src/inner-pages/education/owning-home.php
+++ src/inner-pages/education/owning-home.php
@@ -1,47 +1,12 @@
 <!doctype html>
 <html lang="en">
 
-<head>
-    <title>Platinum FCU - Owning a Home</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="../images/favicon.ico" />
-    <!-- bootstrap -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <!-- font awesome -->
-    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <!-- ionicons icon -->
-    <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
-    <!-- mega menu -->
-    <link href="../css/mega-menu/mega_menu.css" rel="stylesheet" type="text/css" />
-    <!-- owl-carousel -->
-    <link href="../css/owl-carousel/owl.carousel.css" rel="stylesheet" type="text/css" />
-    <!-- magnific popup -->
-    <link href="../css/magnific-popup.css" rel="stylesheet" type="text/css" />
-    <!-- animate -->
-    <link href="../css/animate.css" rel="stylesheet" type="text/css" />
-    <!-- media element player -->
-    <link href="../css/mediaelementplayer.min.css" rel="stylesheet" type="text/css" />
-    <!-- REVOLUTION STYLE SHEETS -->
-    <link href="../revolution/css/settings.css" rel="stylesheet" type="text/css">
-    <!-- ADD-ONS CSS FILES -->
-    <link href="../revolution/css/revolution.addon.particles.css" rel="stylesheet" type="text/css">
-    <!-- shortcodes -->
-    <link href="../css/shortcodes.css" rel="stylesheet" type="text/css" />
-    <!-- main style -->
-    <link href="../css/style.css" rel="stylesheet" type="text/css" />
-    <!-- responsive -->
-    <link href="../css/responsive.css" rel="stylesheet" type="text/css" />
-    <!-- custom -->
-    <link href="../css/custom.css" rel="stylesheet" type="text/css" />
-	<!-- Remodal styles -->
-    <link rel="stylesheet" href="../remodal/dist/remodal.css">
-    <link rel="stylesheet" href="../remodal/dist/remodal-default-theme.css">
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-59046801-1"></script>
-</head>
+<?php $current = "Owning A Home"; ?>
+
+<?php
+$title = $current;
+include '../inc/head.php';
+?>
 
 <body>
     <!-- loading -->
@@ -53,28 +18,21 @@
     <!-- loading End -->
     <!-- HEADER  -->
     <header class="header-06 white">
-        <?php include '../inc/header.php';?>
+        <?php include '../inc/header.php'; ?>
     </header>
     <!-- /HEADER END -->
     <!-- search -->
     <div class="search hidden">
-        <?php include '../inc/site-search.php';?>
+        <?php include '../inc/site-search.php'; ?>
     </div>
     <!-- /search END -->
     <!--======= Breadcrumb Left With BG Image =======-->
-    <section class="overview-block-ptb1 iq-over-black-10 jarallax iq-breadcrumb3 text-left iq-font-white" style="background-color: #575757;">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-12">
-                    <div class="iq-mb-0">
-                        <h2 class="iq-font-white iq-tw-6 custom-header">Education</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php
+    $page_title = $current;
+    include '../inc/about-hero.php';
+    ?>
 		<!--HIDDEN NAV-->
-	<?php include '../inc/education-hidden-nav.php';?>
+	<?php include '../inc/education-hidden-nav.php'; ?>
 
     <!--======= Breadcrumb Left With BG Image =======-->
     <!--=================================
@@ -188,7 +146,7 @@ Tabs -->
             </div>
         </div>
 		<!--HIDDEN GREEN-->
-		<?php include '../inc/hidden-green.php';?>
+		<?php include '../inc/hidden-green.php'; ?>
         <!--=================================
 Tabs -->
     </div>
@@ -197,7 +155,7 @@ Main Content -->
     <!--=================================
 Footer -->
     <footer class="iq-footer6">
-        <?php include '../inc/footer.php';?>
+        <?php include '../inc/footer.php'; ?>
     </footer>
     <!--=================================
 Footer -->
@@ -207,8 +165,8 @@ Footer -->
         <a class="top" id="top" href="#top" aria-label="button to return to top of page"><img src="" alt="Image to go to top of page" style="display:none;"> <i class="ion-ios-arrow-up"></i> </a>
     </div>
     <!-- back-to-top End -->
-	<?php include '../inc/modals-sm.php';?>
-    <?php include '../inc/inner-scripts.php';?>
+	<?php include '../inc/modals-sm.php'; ?>
+    <?php include '../inc/inner-scripts.php'; ?>
 </body>
 
 </html>
