c818e7b26ece5220a09e7446192bef095316bf19
Dillon Ellis <dellis@fcul.com>
2023-04-13 10:38:12 -0400

    loans pages add includes

--- src/inner-pages/loans/business-loan.php
+++ src/inner-pages/loans/business-loan.php
@@ -1,48 +1,12 @@
 <!doctype html>
 <html lang="en">
 
-<head>
-    <title>Platinum FCU - Business Loans</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="../images/favicon.ico" />
-    <!-- bootstrap -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <!-- font awesome -->
-    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <!-- ionicons icon -->
-    <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
-    <!-- mega menu -->
-    <link href="../css/mega-menu/mega_menu.css" rel="stylesheet" type="text/css" />
-    <!-- owl-carousel -->
-    <link href="../css/owl-carousel/owl.carousel.css" rel="stylesheet" type="text/css" />
-    <!-- magnific popup -->
-    <link href="../css/magnific-popup.css" rel="stylesheet" type="text/css" />
-    <!-- animate -->
-    <link href="../css/animate.css" rel="stylesheet" type="text/css" />
-    <!-- media element player -->
-    <link href="../css/mediaelementplayer.min.css" rel="stylesheet" type="text/css" />
-    <!-- REVOLUTION STYLE SHEETS -->
-    <link href="../revolution/css/settings.css" rel="stylesheet" type="text/css">
-    <!-- ADD-ONS CSS FILES -->
-    <link href="../revolution/css/revolution.addon.particles.css" rel="stylesheet" type="text/css">
-    <!-- shortcodes -->
-    <link href="../css/shortcodes.css" rel="stylesheet" type="text/css" />
-    <!-- main style -->
-    <link href="../css/style.css" rel="stylesheet" type="text/css" />
-    <!-- responsive -->
-    <link href="../css/responsive.css" rel="stylesheet" type="text/css" />
-    <!-- custom -->
-    <link href="../css/custom.css" rel="stylesheet" type="text/css" />
-    <!-- Remodal styles -->
-    <link rel="stylesheet" href="../remodal/dist/remodal.css">
-    <link rel="stylesheet" href="../remodal/dist/remodal-default-theme.css">
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-59046801-1"></script>
-    <link rel="stylesheet" href="../css/swiper-banners.css">
-</head>
+<?php $current = "Business Loans"; ?>
+
+<?php
+$title = $current;
+include '../inc/head.php';
+?>
 
 
 <body>
@@ -65,21 +29,10 @@
     </div>
     <!-- /search END -->
     <!--======= Breadcrumb Left With BG Image =======-->
-    <section class="tw-mt-[76px] tw-bg-brandingGreen-400">
-        <div class="">
-            <div
-                class="tw-flex tw-max-w-[1920px] tw-items-center tw-justify-center tw-gap-12 lg:tw-min-h-[250px] lg:tw-justify-start">
-                <img src="../images/inner-header/inner-pattern.png" alt=""
-                    class="tw-absolute tw-hidden tw-h-[250px] tw-w-[675px] tw-object-cover lg:tw-block" srcset="" />
-                <div class="tw-mx-auto tw-flex tw-w-full container tw-px-[15px]">
-                    <h1
-                        class="tw-text-shadow tw-md:text-4xl tw-py-8 tw-font-serif tw-text-2xl tw-font-bold tw-text-white lg:tw-text-5xl">
-                        Business Loans
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php
+    $page_title = $current;
+    include '../inc/about-hero.php';
+    ?>
     <!--HIDDEN NAV-->
     <?php include '../inc/loans-hidden-nav.php'; ?>
     <!--======= Breadcrumb Left With BG Image =======-->
